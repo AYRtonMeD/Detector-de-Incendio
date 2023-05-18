64c8a4297a28aca0b2ca2747ea19f42e191f101a
AYRtonMeD <ayrtondantas2013@gmail.com>
2023-05-18 11:56:05 -0300

    Feitas as modificações no componente de sensor de luz

--- components/light_sensor/include/light_sensor.h
+++ components/light_sensor/include/light_sensor.h
@@ -1,10 +1,5 @@
-static adc1_channel_t _light_sensor_channel;
-static esp_adc_cal_characteristics_t* _adc_chars;
+#define HIGH 1
+#define LOW 0
 
-void configure_light_sensor(const adc1_channel_t channel, esp_adc_cal_characteristics_t* adc_chars);
-
-int read_light_sensor_value();
-
-int read_light_sensor_voltage();
-
-int convert_light_sensor_voltage(int light_sensor_value);
+void light_sensor_init(const int sensor_pin);
+int light_sensor_read(int sensor_pin);
--- components/light_sensor/light_sensor.c
+++ components/light_sensor/light_sensor.c
@@ -1,34 +1,11 @@
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/adc.h"
-#include "esp_adc_cal.h"
-#include <esp_log.h>
+#include "driver/gpio.h"
 #include "light_sensor.h"
 
-#define NO_OF_SAMPLES   64 // number of samples to take for averaging
-
-void configure_light_sensor(const adc1_channel_t channel, esp_adc_cal_characteristics_t* adc_chars) {
-  _light_sensor_channel = channel;
-  _adc_chars = adc_chars;
+void light_sensor_init(const int sensor_pin) {
+  gpio_set_direction(sensor_pin, GPIO_MODE_INPUT);
 }
 
-int read_light_sensor_value() {
-  int adc_reading = 0;
-  for (int i = 0; i < NO_OF_SAMPLES; i++) {
-    adc_reading += adc1_get_raw(_light_sensor_channel);
-    vTaskDelay(1 / portTICK_PERIOD_MS);
-  }
-
-  adc_reading /= NO_OF_SAMPLES;
-
-  return adc_reading;
-}
-
-int read_light_sensor_voltage() {
-  return esp_adc_cal_raw_to_voltage(read_light_sensor_value(), _adc_chars);
-}
-
-int convert_light_sensor_voltage(int light_sensor_value) {
-  return esp_adc_cal_raw_to_voltage(light_sensor_value, _adc_chars);
+int light_sensor_read(int sensor_pin) {
+  return gpio_get_level(sensor_pin);
 }
