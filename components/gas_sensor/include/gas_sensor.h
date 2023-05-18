477159d0d965381163ca0f790083ce9d4b421cad
AYRtonMeD <ayrtondantas2013@gmail.com>
2023-05-18 11:55:34 -0300

    Feitas as modificações no componente de sensor de gás

--- components/gas_sensor/include/gas_sensor.h
+++ components/gas_sensor/include/gas_sensor.h
@@ -1,10 +1,5 @@
-static adc1_channel_t _gas_sensor_channel;
-static esp_adc_cal_characteristics_t* _adc_chars;
+#define HIGH 1
+#define LOW 0
 
-void configure_gas_sensor(const adc1_channel_t channel, esp_adc_cal_characteristics_t* adc_chars);
-
-int read_gas_sensor_value();
-
-int read_gas_sensor_voltage();
-
-int convert_gas_sensor_voltage(int gas_sensor_value);
+void gas_sensor_init(const int sensor_pin);
+int gas_sensor_read(int sensor_pin);
