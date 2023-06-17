196e3a823dec3885ccb95d1d0c1e954c770812a5
AYRtonMeD <ayrtondantas2013@gmail.com>
2023-06-17 17:14:40 -0300

    adicionado conteudo do mqtt_gateway

--- components/mqtt_gateway/include/mqtt_gateway.h
+++ components/mqtt_gateway/include/mqtt_gateway.h
@@ -1,1 +1,9 @@
-void func(void);
+#include "mqtt_client.h"
+
+typedef struct Mqtt_Response {
+  int is_connected;
+} mqtt_response_t;
+
+void mqtt_setup(void);
+esp_mqtt_client_handle_t mqtt_app_start(mqtt_response_t* response);
+mqtt_response_t* build_mqtt_response_struct();
